356f8fb40da7cdcfd5bdcf5f602e254cafb7a6c0
davidsonprata7 <davidson.prata@gmail.com>
2026-02-12 13:24:29 -0300

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,15 +1,1 @@
-import requests
-
-URL = "https://marie-sklodowska-curie-actions.ec.europa.eu/funding/seal-of-excellence"
-
-headers = {
-    "User-Agent": "Mozilla/5.0"
-}
-
-r = requests.get(URL, headers=headers, timeout=30)
-
-print("STATUS:", r.status_code)
-print("TAMANHO HTML:", len(r.text))
-
-print("\nPRIMEIROS 1000 CARACTERES:\n")
-print(r.text[:1000])
+print("TESTE EXECUTANDO CORRETAMENTE")
