cdc9055b66a1d9d2b4bda9b1c063f60e16b38d4c
davidsonprata7 <davidson.prata@gmail.com>
2026-02-12 13:36:09 -0300

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -6,6 +6,10 @@ import xml.etree.ElementTree as ET
 RSS_URL = "https://marie-sklodowska-curie-actions.ec.europa.eu/news/rss.xml"
 STATE_FILE = "state.json"
 
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+}
+
 
 def load_state():
     if not os.path.exists(STATE_FILE):
@@ -20,65 +24,23 @@ def save_state(state):
 
 
 def fetch_rss():
-    r = requests.get(RSS_URL, timeout=30)
+    r = requests.get(RSS_URL, headers=HEADERS, timeout=30)
+
+    print("STATUS CODE:", r.status_code)
+
     if r.status_code != 200:
+        print("RESPONSE TEXT:", r.text[:500])
         raise RuntimeError("Failed to load RSS")
+
     return r.content
 
 
-def send_message(token, chat_id, title, link, pub_date):
-    api = f"https://api.telegram.org/bot{token}/sendPhoto"
-
-    caption = (
-        f"🚩 {title}\n\n"
-        f"⚠️ Publication date:\n"
-        f"✅ {pub_date}"
-    )
-
-    keyboard = {
-        "inline_keyboard": [
-            [{"text": "🔗 Open publication", "url": link}]
-        ]
-    }
-
-    requests.post(
-        api,
-        data={
-            "chat_id": chat_id,
-            "photo": "https://marie-sklodowska-curie-actions.ec.europa.eu/sites/default/files/styles/oe_theme_medium/public/default_images/news-default.jpg",
-            "caption": caption,
-            "reply_markup": json.dumps(keyboard)
-        },
-        timeout=30
-    )
-
-
 def main():
-    token = os.getenv("BOT_TOKEN")
-    chat_id = os.getenv("CHAT_ID")
-
-    state = load_state()
-
-    rss_content = fetch_rss()
-    root = ET.fromstring(rss_content)
+    rss = fetch_rss()
+    root = ET.fromstring(rss)
 
     items = root.findall(".//item")
-
-    print("Total RSS items:", len(items))
-
-    for item in items:
-        title = item.find("title").text
-        link = item.find("link").text
-        pub_date = item.find("pubDate").text
-
-        if "/funding/seal-of-excellence" in link:
-
-            if link not in state["items"]:
-
-                send_message(token, chat_id, title, link, pub_date)
-                state["items"][link] = True
-
-    save_state(state)
+    print("RSS items encontrados:", len(items))
 
 
 if __name__ == "__main__":
